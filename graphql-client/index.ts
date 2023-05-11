5ecb910e0c452e82efe35869fb19753be53a69cb
Klaus Kerstges <klaus.kerstges@gmail.com>
2023-05-11 16:56:35 +0200

    Update GraphQL dependencies.

--- graphql-client/index.ts
+++ graphql-client/index.ts
@@ -3,6 +3,10 @@ import { character, ships } from "./src/starwarsclient";
 (async function() {
     const s = await ships('http://localhost:8080/graphql')
     console.log(s)
+    s.ships.results.forEach(ship => console.log(ship.name))
+
     const c = await character('http://localhost:8080/graphql')
     console.log(c)
+
+    c.characters.results.forEach(char => console.log(char.name))
 }());
